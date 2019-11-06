71c24f162a328c3127af3f8009ca6979c5ccbf58
Clavance Lim <clavance@outlook.com>
2019-11-06 00:50:39 +0000

    Amend app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hellooo world! nanabanannaana
+          Hellooo World!
         </p>
         <a
           className="App-link"
