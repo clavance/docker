5a68780eba656669fc5f205062557bec8e3f5ad9
Clavance Lim <clavance@outlook.com>
2019-11-06 22:24:12 +0000

    amend homepage text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hellooo World!
+          Hellooooooo World!
         </p>
         <a
           className="App-link"
